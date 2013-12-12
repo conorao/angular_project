cf772e22f29facf3050fe93a0b43b650ab0649c1
conorao <conorao@porygon.cyberonic.org>
2013-12-12 14:36:14 +0000

    The MBTA thing works again?

--- app/js/mbta.js
+++ app/js/mbta.js
@@ -7,7 +7,9 @@ function mbtaData($scope, $http) {
     $scope.loadTrains = function() {
     var mainInfo = null;
     
-    $http.get('http://conorodell.com/angular_project/app/Red.json').success(function(data) {
+    //$http.get('http://conorodell.com/angular_project/app/Red.json').success(function(data) {
+    $http.get('http://developer.mbta.com/Data/Red.json').success(function(data) {
+    // this is only here until the MBTA complains again but I think it is a good resource
         mainInfo = data;
         console.log(mainInfo);
     
